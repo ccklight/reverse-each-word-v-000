7d385efeea02e3ac014aa76fbbac8c9fb01a3af6
Cheryl K <cherylkelson@icloud.com>
2018-09-17 18:03:52 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
-# def reverse_each_word(sentence)
-#   sentence.split.collect do {|word|word.reverse}.join (" ")
-# end
+def reverse_each_word(sentence)
+  sentence.split.collect do {|word|word.reverse}.join (" ")
+end
   
 
 
