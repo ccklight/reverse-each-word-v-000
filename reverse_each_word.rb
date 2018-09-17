5d0735a996b301ecfdd0a9ebd719e16d20159e69
Cheryl K <cherylkelson@icloud.com>
2018-09-17 18:02:45 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,18 +7,10 @@ end
 
 
 
-def reverse_each_word(sentence)
-  sentence.collect do{word|.reverse}. join (" ")
-end
-
-
-
 
 def reverse_each_word(sentence)
 sentence.split.collect {|word|word.reverse}.join (" ")
 
 
 def reverse_each_word(sentence)
-sentence.split.collect do |word|
-  word.reverse
-end.join(" ")
+sentence.split.collect do |word|word.reverse.end.join(" ")
